2f02f5363f0e56619f123c4ad563c56a8801a64b
Marcel Amiach <ma.amiach@gmail.com>
2017-01-21 16:25:06 +0200

    [update] Adds list of unwanted words (back list)

--- app/utils/frequencyHandler.js
+++ app/utils/frequencyHandler.js
@@ -1,4 +1,8 @@
 function FrequencyHandler() {
+  //setting unwanted words
+  var unwantedWords = ["a", "an", "and", "the", "is", "To", "Of", "in", "on", "at", "with", "into", "for", "by"];
+  var listSize = 10;
+  
   var order = function(list) {
     //ordering by frequencies
     list.sort(function(a, b) {
