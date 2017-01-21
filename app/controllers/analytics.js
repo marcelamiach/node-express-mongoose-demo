9382b0ac1255e56251c71da49669c7ae2648fa7a
Marcel Amiach <ma.amiach@gmail.com>
2017-01-21 16:28:01 +0200

    [update] Adds function to order list by tag (alphabetically)

--- app/controllers/analytics.js
+++ app/controllers/analytics.js
@@ -10,3 +10,14 @@ const { respond } = require('../utils');
 const Article = mongoose.model('Article');
 
 var Handler = require('../utils/frequencyHandler.js');
+
+function orderByTag(frequencies) {
+  return frequencies.sort(function(a, b) {
+    var firstTag = a.tag.toLowerCase(), secondTag = b.tag.toLowerCase();
+      if (firstTag < secondTag)
+          return -1 
+      if (firstTag > secondTag)
+          return 1
+      return 0
+  });
+}
