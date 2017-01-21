4fb0289cb9d6ba356eb3dcfec9afc4b1f14b91be
Marcel Amiach <ma.amiach@gmail.com>
2017-01-21 16:24:02 +0200

    [update] Adds function to order list of words by high to low frequencies and to filter top 10

--- app/utils/frequencyHandler.js
+++ app/utils/frequencyHandler.js
@@ -1,4 +1,17 @@
 function FrequencyHandler() {
+  var order = function(list) {
+    //ordering by frequencies
+    list.sort(function(a, b) {
+      return b.frequency - a.frequency;
+    });
+
+    //filtering top 10
+    list = list.filter(function(element, index) {
+      return index <= (listSize - 1);
+    });
+
+    return list;
+  }
 }
 
 module.exports = FrequencyHandler;
